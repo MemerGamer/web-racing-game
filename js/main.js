c251885ae65fba24876301fee37117d118577c74
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2022-07-07 15:24:30 +0300

    bugfix and code formatting

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // Global Constants
 
 // Screen Size
-const SCREEN_W= 1920;
+const SCREEN_W = 1920;
 const SCREEN_H = 1080;
 
 // Coordinates of the screen center
-const SCREEN_CX = SCREEN_W/2;
-const SCREEN_CY = SCREEN_H/2;
+const SCREEN_CX = SCREEN_W / 2;
+const SCREEN_CY = SCREEN_H / 2;
 
 // Game States
 const STATE_INIT = 1;
@@ -18,90 +18,109 @@ const STATE_GAMEOVER = 4;
 let state = STATE_INIT;
 
 // Main Scene
-class MainScene extends Phaser.Scene{
-    constructor(){
-        super({key: 'SceneMain'});
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "SceneMain" });
+  }
+  // loads all assets
+  preload() {
+    this.load.image("imgBack", "../assets/img_back.png");
+  }
+  // creates all objects
+  create() {
+    // backgrounds
+    this.sprBack = this.add.image(SCREEN_CX, SCREEN_CY, "imgBack");
+
+    // instances
+    this.circuit = new Circuit(this);
+    this.settings = new Settings(this);
+
+    // listener to pause game
+    this.input.keyboard.on(
+      "keydown-C",
+      function () {
+        console.log("Game is paused. Press [C] to resume. ");
+        this.settings.txtPause.text = "[C] Resume";
+        this.scene.pause();
+        this.scene.launch("ScenePause");
+      },
+      this
+    );
+
+    // listener on resume event
+    this.events.on(
+      "resume",
+      function () {
+        console.log("Game is resumed. Press [C] to pause.");
+        this.settings.show();
+      },
+      this
+    );
+  }
+  // main game loop
+  update(time, delta) {
+    switch (state) {
+      case STATE_INIT:
+        console.log("Init game.");
+        state = STATE_RESTART;
+        break;
+
+      case STATE_RESTART:
+        console.log("Restart game.");
+
+        this.circuit.create();
+
+        state = STATE_PLAY;
+        break;
+
+      case STATE_PLAY:
+        console.log("Playing game.");
+
+        this.circuit.render2D();
+
+        state = STATE_GAMEOVER;
+        break;
+
+      case STATE_GAMEOVER:
+        console.log("Game over.");
+        break;
     }
-    // loads all assets
-    preload(){
-        this.load.image("imgBack","../assets/img_back.png");
-    }
-    // creates all objects
-    create(){
-        // backgrounds
-        this.sprBack = this.add.image(SCREEN_CX,SCREEN_CY,'imgBack');
-
-        // instances
-        this.settings = new Settings(this);
-
-        // listener to pause game
-        this.input.keyboard.on('keydown-P', function(){
-            console.log("Game is paused. Press [P] to resume. ");
-            this.settings.txtPause.text = "[P] Resume";
-            this.scene.pause();
-            this.scene.launch('ScenePause');
-        },this);
-
-        // listener on resume event
-        this.events.on('resume',function(){
-            console.log("Game is resumed. Press [P] to pause.");
-            this.settings.show();
-        },this);
-    }
-    // main game loop
-    update(time,delta){
-        switch(state){
-            case STATE_INIT:
-                console.log("Init game.");
-                state = STATE_RESTART;
-                break;
-
-            case STATE_RESTART:
-                console.log("Restart game.");
-                state = STATE_PLAY;
-                break;
-
-            case STATE_PLAY:
-                console.log("Playing game.");
-                state = STATE_GAMEOVER;
-                break;
-
-            case STATE_GAMEOVER:
-                console.log("Game over.");
-                break;
-        }
-    }
+  }
 }
 
 // Pause Scene
-class PauseScene extends Phaser.Scene{
-    constructor(){
-        super({key: 'ScenePause'});
-    }
-    
-    create(){
-        // listener to resume game
-        this.input.keyboard.on('keydown-P', function(){
-            this.scene.resume('SceneMain');
-            this.scene.stop();
-        },this);
-    }
+class PauseScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "ScenePause" });
+  }
+
+  create() {
+    // listener to resume game
+    this.input.keyboard.on(
+      "keydown-C",
+      function () {
+        this.scene.resume("SceneMain");
+        this.scene.stop();
+      },
+      this
+    );
+  }
 }
 
 // Initializing Phaser Game
 
 // Game Configuration
 let config = {
-    type: Phaser.AUTO,
-    width: SCREEN_W,
-    height: SCREEN_H,
+  type: Phaser.AUTO,
+  width: SCREEN_W,
+  height: SCREEN_H,
 
-    scale:{
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-    },
-    scene: [MainScene,PauseScene]
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [MainScene, PauseScene],
 };
 
 // Game Instance
-let game = new Phaser.Game(config)
+let game = new Phaser.Game(config);
--- js/settings.js
+++ js/settings.js
@@ -4,7 +4,7 @@ class Settings{
         this.scene = scene;
 
         var font = {font: '32px Arial', fill: '#000000'};
-        this.txtPause = scene.add.text{1720,5, '', font};
+        this.txtPause = scene.add.text(1720,5, '', font);
 
         this.show();
     }
@@ -12,6 +12,6 @@ class Settings{
     // shows settings.
 
     show(){
-        this.txtPause.text = "[P] Pause";
+        this.txtPause.text = "[C] Pause";
     }
 }
